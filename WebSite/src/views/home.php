0dcdf47158817d08cf0f5ecf4b491d92bc4d0b15
Grammar-Programmer <acmoz@iscte-iul.pt>
2024-04-20 22:37:46 +0100

    Adding login view, modal. Rewriting login.php

---
+++ WebSite/src/views/home.php
@@ -0,0 +1,45 @@
+<?php
+session_start();
+session_cache_limiter('Cache-Control : no-store, no-cache, must-revalidate, post-check=0, pre-check=0');
+
+if (!isset($_SESSION['id'])) {
+    // Redirect to the login page or display an error message
+    header('Location: ../login/index.php');
+    exit();
+}
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" type="text/css" href="../style.css">
+    <script type="text/javascript" src="../main.js"></script>
+    <title>Home</title>
+</head>
+<body>
+    <?php
+    $username = $_SESSION['username'];
+    $usertype = $_SESSION['usertype'];
+    ?>
+
+    <nav>
+        <div class="main-nav-container">
+            <a href="home.php">Home</a>
+            <a href="experiences.php">Experiencias</a>
+        </div>
+        <div class="logout-container">
+            <a href="../login/index.php">Log Out</a>
+        </div>
+    </nav>
+
+    <div>
+        <h1>Painel de Controlo</h1>
+
+        <p>Bem vindo de volta <?php echo $usertype ?> <?php echo $username ?> !</p>
+    </div>
+
+</body>
+</html>
