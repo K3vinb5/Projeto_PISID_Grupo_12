e27c67065fd1eb3264c8d500e07ff1573a4c0011
Kevin Rodrigues Borges <115191440+K3vinb5@users.noreply.github.com>
2024-05-06 03:36:58 +0100

    flutter temps finished

--- WebSite/db/db_getTemp2.php
+++ WebSite/db/db_getTemp2.php
@@ -7,4 +7,27 @@ $url = "127.0.0.1";
 $database = "grupo12_bd";
 $username = $_POST['username'];
 $password = $_POST['password'];
+$time = $_POST['time'];
 
+//$username = "root";
+//$password = "";
+//$time = 3;
+
+$conn = mysqli_connect($url, $username, $password, $database);
+
+$sql = "SELECT  mt.DataHora, mt.Leitura FROM medicoestemperatura mt, sensor s, tiposensor ts WHERE mt.Sensor = s.IDSensor AND s.IDTipoSensor = ts.IDTipoSensor AND s.Nome = '2' AND ts.Designacao = 'Temperatura' AND mt.DataHora >= NOW() - INTERVAL $time MINUTE;;";
+$result = mysqli_query($conn, $sql);
+$readings = "[";
+
+$row_count = $result->num_rows;
+$counter = 0;
+while ($row = $result->fetch_assoc()) {
+    $counter++;
+    if ($counter == $row_count) {
+        $readings .= json_encode($row)."]";
+    }else{
+        $readings .= json_encode($row).",";
+    }
+}
+
+echo "{\"readings\": ".$readings."}";
--- flutter_app/lib/screens/bottom_navigation_screen.dart
+++ flutter_app/lib/screens/bottom_navigation_screen.dart
@@ -2,21 +2,21 @@ import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
 class BottomNavigationBarScreen extends StatefulWidget {
-  const BottomNavigationBarScreen({Key? key, required this.navigationShell}) : super(key: key);
+  const BottomNavigationBarScreen({Key? key, required this.navigationShell})
+      : super(key: key);
 
   final StatefulNavigationShell navigationShell;
 
-
   @override
-  State<BottomNavigationBarScreen> createState() => _BottomNavigationBarScreenState();
+  State<BottomNavigationBarScreen> createState() =>
+      _BottomNavigationBarScreenState();
 }
 
 class _BottomNavigationBarScreenState extends State<BottomNavigationBarScreen> {
-
   int selectedIndex = 0;
   List<String> appBarTitle = ["Alerts", "Mice / Rooms", "Sensor 1", "Sensor 2"];
 
-  void onBottomNavigationBarTap(/*BuildContext context, */int index) {
+  void onBottomNavigationBarTap(/*BuildContext context, */ int index) {
     setState(() {
       selectedIndex = index;
     });
@@ -29,39 +29,42 @@ class _BottomNavigationBarScreenState extends State<BottomNavigationBarScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(appBarTitle[selectedIndex]),
-        centerTitle: true,
-        backgroundColor: Colors.blue,
-      ),
-      body: widget.navigationShell,
+        appBar: AppBar(
+          title: Text(appBarTitle[selectedIndex]),
+          centerTitle: true,
+          foregroundColor: Colors.white,
+          backgroundColor: Colors.blue,
+          actions: [
+          ],
+        ),
+        body: widget.navigationShell,
         bottomNavigationBar: BottomNavigationBar(
-            iconSize:30,
-            selectedFontSize:16,
-            unselectedFontSize:16,
+            iconSize: 30,
+            selectedFontSize: 16,
+            unselectedFontSize: 16,
             showSelectedLabels: true,
             showUnselectedLabels: true,
             items: const [
               BottomNavigationBarItem(
-                icon:Icon(Icons.crisis_alert),
-                label:'Alerts',
-                backgroundColor:Colors.blue,
+                icon: Icon(Icons.crisis_alert),
+                label: 'Alerts',
+                backgroundColor: Colors.blue,
               ),
               BottomNavigationBarItem(
-                icon:Icon(Icons.gesture),
-                label:'Mice/Room',
-                backgroundColor:Colors.blue,
+                icon: Icon(Icons.gesture),
+                label: 'Mice/Room',
+                backgroundColor: Colors.blue,
               ),
               BottomNavigationBarItem(
-                icon:Icon(Icons.sensors),
-                label:'Temp Sensor 1',
-                backgroundColor:Colors.blue,
+                icon: Icon(Icons.sensors),
+                label: 'Temp Sensor 1',
+                backgroundColor: Colors.blue,
               ),
               BottomNavigationBarItem(
-                icon:Icon(Icons.sensors),
+                icon: Icon(Icons.sensors),
                 //<ion-icon name="thermometer-outline"></ion-icon>
-                label:'Temp Sensor 2',
-                backgroundColor:Colors.blue,
+                label: 'Temp Sensor 2',
+                backgroundColor: Colors.blue,
               ),
             ],
             type: BottomNavigationBarType.shifting,
@@ -69,8 +72,6 @@ class _BottomNavigationBarScreenState extends State<BottomNavigationBarScreen> {
             selectedItemColor: Colors.black,
             //iconSize: 40,
             onTap: onBottomNavigationBarTap,
-            elevation: 5
-        )
-    );
+            elevation: 5));
   }
 }
