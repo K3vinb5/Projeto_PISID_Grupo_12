fbcbb736c62d48b7f61ea42c30bb8387913dce93
Kevin Rodrigues Borges <115191440+K3vinb5@users.noreply.github.com>
2024-05-16 07:45:52 +0100

    Some deletions

--- Scripts/MqttToSQL(not working).bat
+++
@@ -1,1 +0,0 @@
-java -cp "./org.eclipse.paho.client.mqttv3-1.1.0.jar;./mongo-java-driver-3.12.14.jar;./bson-4.11.0.jar;./mongodb-driver-sync-4.0.0.jar;./mariadb-java-client-3.3.3.jar;./mysql-connector-j-8.3.0.jar;./gson-2.10.1.jar;." mainLauncher.FatherMain ./conf/ReceiveCloud.ini insertSQL.ReceiveCloud
